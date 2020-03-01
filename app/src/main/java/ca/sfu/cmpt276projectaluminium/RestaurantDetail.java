1aa6d5be901a2f435d86d12b460c20c9b50fbd4f
kanolano <worldingly@gmail.com>
2020-02-29 23:37:17 -0800

    Initial Commit For Branch
    Placed the basic ui layout
    Will complete later

---
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/RestaurantDetail.java
@@ -0,0 +1,14 @@
+package ca.sfu.cmpt276projectaluminium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+    }
+}
