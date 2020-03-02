98ade8ec1541d6656e69e87555951457d0e27444
Jarod <jarod_forbes@sfu.ca>
2020-03-01 22:24:53 -0800

    Implement basics of Violation.java

--- app/src/main/java/ca/sfu/cmpt276projectaluminium/model/Violation.java
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/model/Violation.java
@@ -4,5 +4,25 @@ package ca.sfu.cmpt276projectaluminium.model;
  * Manages data about violations by storing it and providing getters in an organized manner
  */
 public class Violation {
+    private int ID;
+    private String severity;  // Critical vs non-critical
+    private String description;  // A full description (including section number)
 
+    Violation(int ID, String severity, String description) {
+        this.ID = ID;
+        this.severity = severity;
+        this.description = description;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
