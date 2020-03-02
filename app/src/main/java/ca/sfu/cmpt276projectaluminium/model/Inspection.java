9661d69d1482ae18711b531c19995aeb91a9f3f6
Jarod <jarod_forbes@sfu.ca>
2020-03-01 22:26:02 -0800

    Add getters for violations and numTotalViolations

--- app/src/main/java/ca/sfu/cmpt276projectaluminium/model/Inspection.java
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/model/Inspection.java
@@ -1,5 +1,7 @@
 package ca.sfu.cmpt276projectaluminium.model;
 
+import java.util.ArrayList;
+
 /**
  * Manages data about inspection reports by storing it and providing getters in an organized manner
  */
@@ -19,7 +21,7 @@ public class Inspection {
     /**
      * Constructor is package private as it should only be called by InspectionManager
      */
-    public Inspection(String trackingNumber, String inspectionDate, String type, String hazardRating,
+    Inspection(String trackingNumber, String inspectionDate, String type, String hazardRating,
                       int numCriticalViolations, int numNonCriticalViolations) {
         this.trackingNumber = trackingNumber;
         this.inspectionDate = inspectionDate;
@@ -51,5 +53,21 @@ public class Inspection {
 
     public int getNumNonCriticalViolations() {
         return numNonCriticalViolations;
+    }
+
+    public int getNumTotalViolatinos() {
+        return numCriticalViolations + numNonCriticalViolations;
+    }
+
+    /**
+     * Currently returns a hard-coded example set of data as it's unimplemented.
+     */
+    public ArrayList<Violation> getViolations() {
+        ArrayList<Violation> violations = new ArrayList<>();
+        violations.add(new Violation(101, "Not Critical", "Plans/construction/alterations not in accordance with the Regulation [s. 3; s. 4]"));
+        violations.add(new Violation(404, "Not Critical", "Employee smoking in food preparation/processing/storage areas [s. 21(2)]"));
+        violations.add(new Violation(203, "Critical", "Food not cooled in an acceptable manner [s. 12(a)]"));
+
+        return violations;
     }
 }
