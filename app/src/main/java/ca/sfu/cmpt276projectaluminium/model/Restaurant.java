24d9e160cbb8f8575e3b8a0f77e6ad39b82243cd
Jarod <jarod_forbes@sfu.ca>
2020-03-01 16:28:16 -0800

    Initial data class commits

--- app/src/main/java/ca/sfu/cmpt276projectaluminium/model/Restaurant.java
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/model/Restaurant.java
@@ -1,8 +1,8 @@
 package ca.sfu.cmpt276projectaluminium.model;
 
 /**
- * Manages data about restaurants by storing it and providing getters in an organized manner
+ * Contains the data about a restaurant and provides getters in an organized manner
  */
 public class Restaurant {
-
+    
 }
---
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/model/RestaurantManager.java
@@ -0,0 +1,8 @@
+package ca.sfu.cmpt276projectaluminium.model;
+
+/**
+ * Manages data about restaurants by storing them all in an easily accessible list
+ */
+public class RestaurantManager {
+
+}
