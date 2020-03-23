aa8e9364ceb74104c23559565729841a7a0df44f
Jarod <jarod_forbes@sfu.ca>
2020-03-22 19:56:06 -0700

    Add ability to chance individual icons

--- app/src/main/java/ca/sfu/cmpt276projectaluminium/UI/MainActivity.java
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/UI/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -23,11 +24,15 @@ import ca.sfu.cmpt276projectaluminium.model.RestaurantManager;
 import java.util.ArrayList;
 import java.util.List;
 
+// Sources:
+// https://stackoverflow.com/questions/5089300/how-can-i-change-the-image-of-an-imageview
+// https://stackoverflow.com/questions/36630428/listview-reusing-old-images - Fragments are reused
+// https://stackoverflow.com/questions/2275004/in-java-how-do-i-check-if-a-string-contains-a-substring-ignoring-case
+
 /**
  * Displays a list of restaurants and some info on the most most recent inspection report for
  * each of the restaurants displayed
  */
-
 public class MainActivity extends AppCompatActivity {
 
     private RestaurantManager manager = RestaurantManager.getInstance();
@@ -94,6 +99,76 @@ public class MainActivity extends AppCompatActivity {
             super(MainActivity.this, R.layout.restaurants_view, restaurantArray);
         }
 
+        /**
+         * This function takes a restaurant name and finds it's corresponding icon.
+         * It then returns the android resource id of the icon
+         * @param s The restaurant name
+         * @return The restaurant icon resource id number
+         */
+        private int getDrawableID(String s) {
+            String restaurantName = s.toLowerCase();
+
+            if (restaurantName.contains("7-eleven")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("a&w") || restaurantName.contains("a & w")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("blenz")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("booster juice")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("boston pizza")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("burger king")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("church's chicken")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("cobs bread")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("d-plus pizza")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("dairy queen")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("domino's pizza")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("freshii")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("freshslice")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("orange julius")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("panago")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("papa john's")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("pizza hut")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("quesada")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("quizno's") || restaurantName.contains("quiznos")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("real canadian superstore")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("safeway")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("save on foods")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("starbucks coffee")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("subway")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("t&t")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("tim horton's") || restaurantName.contains("tim hortons")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("wendy's")) {
+                return R.drawable.cooking;
+            } else if (restaurantName.contains("white spot")) {
+                return R.drawable.cooking;
+            } else {
+                return R.drawable.cooking;
+            }
+        }
+
         @NonNull
         @Override
         //display image bases on position
@@ -139,6 +214,15 @@ public class MainActivity extends AppCompatActivity {
                 hazardImage.setImageResource(R.drawable.not_available);
                 itemView.setBackground(getDrawable(R.drawable.border_blue));
             }
+
+            // Fetch the imageView so we can later set it to a restaurant icon
+            ImageView restaurantIcon = itemView.findViewById(R.id.iconRestaurant);
+
+            // Get the id of the image we want to use, depending on the restaurant name
+            int drawableID = getDrawableID(currentRestaurant.getName());
+
+            // Set imageView to restaurant icon. If restaurant doesn't have icon, use default one
+            restaurantIcon.setImageResource(drawableID);
 
             // display address
             TextView addressTxt = itemView.findViewById(R.id.txtAddress);
