b9926a3d992667e51a95529fab83ee183d999afb
nathansimms <nsimms@sfu.ca>
2020-03-27 23:14:08 -0700

    Update time till locationListner updates

--- app/src/main/java/ca/sfu/cmpt276projectaluminium/UI/MapsActivity.java
+++ app/src/main/java/ca/sfu/cmpt276projectaluminium/UI/MapsActivity.java
@@ -354,9 +354,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void requestLocationUpdates() {
         myLocListener = new MyLocationListener();
         // The minimum time (in milliseconds) the system will wait until checking if the location changed
-        int minTime = 10000;
+        int minTime = 5000;
         // The minimum distance (in meters) traveled until notified
-        float minDistance = 2;
+        float minDistance = 1;
         // Get the location manager from the system
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         // Get the best provider from the criteria specified, and false to say it can turn the provider on if it isn't already
